492275f20ad8bf06cbf55cd55c1063a3851a3fe1
IzanBlanes <izanblanesaracil@gmail.com>
2024-02-24 13:42:20 +0100

    Modificado la clase ClientWorker

--- TCPEchoServer/src/main/java/es/cipfpbatoi/dam/psp/ud6/sockets/ClientWorker.java
+++ TCPEchoServer/src/main/java/es/cipfpbatoi/dam/psp/ud6/sockets/ClientWorker.java
@@ -2,58 +2,62 @@ package es.cipfpbatoi.dam.psp.ud6.sockets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class ClientWorker implements Runnable {
-
     Socket socket;
+    String nombreCliente;
+    private static final String PALABRA_FINALIZAR_COMUNICACION = "bye";
+    ArrayList<ClientWorker> clientWorkers;
+    PrintWriter outToClient;
 
-    public ClientWorker(Socket socket) {
+    public ClientWorker(Socket socket, String nombreCliente, ArrayList<ClientWorker> clientWorkers) {
         this.socket = socket;
+        this.nombreCliente = nombreCliente;
+        this.clientWorkers = clientWorkers;
     }
 
     @Override
     public void run() {
-        //Ejecutar varios sockets, hacer lo mismo que en la clase TCPEchoServer
-        int port = 6789; // Puerto en el que el servidor escuchará
+        String frase = "";
+        while (!frase.equalsIgnoreCase(PALABRA_FINALIZAR_COMUNICACION)) {
+            try {
+                Scanner inFromClient = new Scanner(socket.getInputStream());
+                outToClient = new PrintWriter(socket.getOutputStream(), true);
 
-//        try (ServerSocket serverSocket = new ServerSocket(port)) {
-//            System.out.println("Servidor iniciado y escuchando en el puerto " + port);
-//
-//            // El servidor se queda en un bucle infinito esperando por conexiones de clientes
-//            while (true) {
+                // Recibe el mensaje del cliente
+                String clientSentence = inFromClient.nextLine();
+                frase = clientSentence;
+                // Procesa el mensaje y responde
+                String capitalizedSentence = nombreCliente + ": " + clientSentence + System.lineSeparator();
 
-        try (// Espera una conexión de un cliente
+                for (ClientWorker cliente : clientWorkers) {
+                    if (!cliente.getNombreCliente().equalsIgnoreCase(this.nombreCliente)) {
+                        cliente.outToClient.println(capitalizedSentence);
+                    }
+                }
+                outToClient.println(capitalizedSentence);
 
-             // Crea flujos de entrada y salida para leer y escribir datos hacia/desde el cliente
-             Scanner inFromClient = new Scanner(socket.getInputStream());
-             PrintWriter outToClient = new PrintWriter(socket.getOutputStream(), true)) {
-
-
-            // Recibe el mensaje del cliente
-            String clientSentence = inFromClient.nextLine();
-
-            // Procesa el mensaje y responde
-            String capitalizedSentence = "El servidor ha recibido: " + clientSentence.toUpperCase() + System.lineSeparator();
-
-            outToClient.println(capitalizedSentence);
-
-//                } catch (IOException e){
-//                    System.err.println(e.getLocalizedMessage());
-//                }
-//
-//            }
-//        } catch (IOException e) {
-//            System.err.println(e.getLocalizedMessage());
-//        }
+            } catch (IOException e) {
+                System.err.println(e.getLocalizedMessage());
+            }
+        }
+        for (ClientWorker cliente : clientWorkers) {
+            if (!cliente.getNombreCliente().equalsIgnoreCase(this.nombreCliente))
+                cliente.outToClient.println(this.nombreCliente+ " a abandonado el chat");
+        }
+        try {
+            socket.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-//        socket.close();
+
+    }
+
+    public String getNombreCliente() {
+        return nombreCliente;
     }
 }
 
--- TCPEchoServer/src/main/java/es/cipfpbatoi/dam/psp/ud6/sockets/TCPEchoServer.java
+++ TCPEchoServer/src/main/java/es/cipfpbatoi/dam/psp/ud6/sockets/TCPEchoServer.java
@@ -1,44 +1,45 @@
 package es.cipfpbatoi.dam.psp.ud6.sockets;
 
-import java.io.*;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Locale;
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 
 public class TCPEchoServer {
     public static void main(String[] args) throws IOException {
 
-        int port = 6789; // Puerto en el que el servidor escuchará
-
+        int port = 6666; // Puerto en el que el servidor escuchará
+        int totalNucleos = Runtime.getRuntime().availableProcessors();
+        ThreadPoolExecutor poolExecutor = (ThreadPoolExecutor) Executors.newFixedThreadPool(totalNucleos);
+        ArrayList<ClientWorker> clientes = new ArrayList<>();
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             System.out.println("Servidor iniciado y escuchando en el puerto " + port);
 
-            // El servidor se queda en un bucle infinito esperando por conexiones de clientes
             while (true) {
+                Socket connectionSocket = serverSocket.accept();
+                // Crea flujos de entrada y salida para leer y escribir datos hacia/desde el cliente
+                Scanner inFromClient = new Scanner(connectionSocket.getInputStream());
+                String clientName = inFromClient.nextLine();
+//                    PrintWriter outToClient = new PrintWriter(connectionSocket.getOutputStream(), true);
+                ClientWorker clientWorker = new ClientWorker(connectionSocket, clientName, clientes);
+                Thread t = new Thread(clientWorker);
+                t.start();
+                clientes.add(clientWorker);
+                poolExecutor.getQueue().add(clientWorker);
 
-                try (// Espera una conexión de un cliente
-                     Socket connectionSocket = serverSocket.accept();
-                     // Crea flujos de entrada y salida para leer y escribir datos hacia/desde el cliente
-                     Scanner inFromClient = new Scanner(connectionSocket.getInputStream());
-                     PrintWriter outToClient = new PrintWriter(connectionSocket.getOutputStream(),true)){
-
-
-                    // Recibe el mensaje del cliente
-                    String clientSentence = inFromClient.nextLine();
-
-                    ClientWorker clientWorker = new ClientWorker(connectionSocket);
-                    Thread t = new Thread(clientWorker);
-                    t.start();
-
-                    // Procesa el mensaje y responde
-                    String capitalizedSentence = "El servidor ha recibido: " + clientSentence.toUpperCase() + System.lineSeparator();
-
-                    outToClient.println(capitalizedSentence);
-
-                } catch (IOException e){
-                    System.err.println(e.getLocalizedMessage());
+                for (ClientWorker cliente : clientes) {
+                    if (!cliente.getNombreCliente().equalsIgnoreCase(clientName)) {
+                        cliente.outToClient.println(clientWorker.getNombreCliente() + " se ha unido al chat");
+                    }
                 }
+
+                // Procesa el mensaje y responde
+//                        String capitalizedSentence = "El servidor ha recibido: " + clientSentence.toUpperCase() + System.lineSeparator();
+
+//                        outToClient.println(capitalizedSentence);
 
             }
         } catch (IOException e) {
